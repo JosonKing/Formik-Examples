8c389d110341f84668378aae5255c35365ce371e
josonking <wangsanmu5@163.com>
2022-08-17 22:47:19 +0800

    function children with errors messages

--- src/index.js
+++ src/index.js
@@ -92,42 +92,48 @@ const SignupForm = () => {
           }, 400);
         }}
       >
-        <Form>
-          <MyTextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="Jane"
-          />
+        {({errors, touched}) => {
+          console.log('errors:', errors, 'touched:', touched)
+          return (
 
-          <MyTextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Doe"
-          />
-
-          <MyTextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="jane@formik.com"
-          />
-
-          <MySelect label="Job Type" name="jobType">
-            <option value="">Select a job type</option>
-            <option value="designer">Designer</option>
-            <option value="development">Developer</option>
-            <option value="product">Product Manager</option>
-            <option value="other">Other</option>
-          </MySelect>
-
-          <MyCheckbox name="acceptedTerms">
-            I accept the terms and conditions
-          </MyCheckbox>
-
-          <button type="submit">Submit</button>
-        </Form>
+            <Form>
+              <MyTextInput
+                label="First Name"
+                name="firstName"
+                type="text"
+                placeholder="Jane"
+              />
+  
+              <MyTextInput
+                label="Last Name"
+                name="lastName"
+                type="text"
+                placeholder="Doe"
+              />
+  
+              <MyTextInput
+                label="Email Address"
+                name="email"
+                type="email"
+                placeholder="jane@formik.com"
+              />
+  
+              <MySelect label="Job Type" name="jobType">
+                <option value="">Select a job type</option>
+                <option value="designer">Designer</option>
+                <option value="development">Developer</option>
+                <option value="product">Product Manager</option>
+                <option value="other">Other</option>
+              </MySelect>
+  
+              <MyCheckbox name="acceptedTerms">
+                I accept the terms and conditions
+              </MyCheckbox>
+  
+              <button type="submit">Submit</button>
+            </Form>
+          )
+        }}
       </Formik>
     </>
   );
